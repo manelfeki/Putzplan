a1d1399440e1e7f708a85dd325b84adcf3e0dc2c
Camarchepas <36743875+Camarchepas@users.noreply.github.com>
2020-01-21 10:28:12 +0100

    Changes on Task : now take into account repeatable task

--- src/backend/Task.js
+++ src/backend/Task.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+var Resident = require('./Resident');
 
 //Create schema and model
 const Schema = mongoose.Schema;
@@ -8,17 +8,27 @@ const TaskSchema = new Schema({
     description: {
         type: String
     },
+    // in non repeatable task : AssignedResident is the the only resident
+    //in repeatable task : AssignedResident is the current Resident 
     assignedResident: {
         type: mongoose.Schema.Types.ObjectId
-    },
-    isDone: {
-        type: Boolean
     },
     startDate: {
         type: Date
     },
     endDate: {
         type: Date
+    },
+    isRepeating: {
+        type: Boolean
+    },
+    index: {
+        type: Number,
+        default: 0
+    },
+    //in repeatable task : contains all the assigned residents
+    assignedResidents: {
+        type: [mongoose.Schema.Types.ObjectId]
     }
 
 });
